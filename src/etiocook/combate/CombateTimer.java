716cbf1692e0811368e83f0eb9c69414ccaf28da
CassyusKley <eTioCooK99@gmail.com>
2020-05-07 18:07:09 -0300

    ECombate

--- src/etiocook/combate/CombateTimer.java
+++ src/etiocook/combate/CombateTimer.java
@@ -1,12 +1,9 @@
 package etiocook.combate;
 
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 public class CombateTimer extends BukkitRunnable {
@@ -21,7 +18,9 @@ public class CombateTimer extends BukkitRunnable {
                 if (Main.getInstance().getManager().get(player.getName()).getDelay() >= System.currentTimeMillis()) {
 
                     long t = Main.getInstance().getManager().get(player.getName()).getDelay() - System.currentTimeMillis();
-                    Main.getInstance().sendBar(player, " §c" +TimeUnit.MILLISECONDS.toSeconds(t));
+                    Main.getInstance().sendBar(
+                            player,
+                            " §c" +TimeUnit.MILLISECONDS.toSeconds(t) + "segundos");
 
                 } else {
 
